2450129ee54a95c5595f3476bcd8f06ab4295864
Flier Lu <flier.lu@gmail.com>
2016-04-02 01:09:09 +0800

    build boost from source

--- .travis/setup-travis.sh
+++ .travis/setup-travis.sh
@@ -4,8 +4,16 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew update
-  brew install cmake boost
+  brew outdated cmake || brew upgrade cmake
+  brew outdated boost || brew upgrade boost
 else
   sudo apt-get update -qq
-  sudo apt-get install cmake libboost-dev
+  sudo apt-get install cmake 
+
+  wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz -O /tmp/boost.tar.gz
+  tar -xvf /tmp/boost.tar.gz
+  pushd boost_1_60_0
+  ./bootstrap.sh
+  sudo ./b2 install -j 2 --prefix=/usr link=static
+  popd
 fi
