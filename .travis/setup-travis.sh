975e435c474370043e7480320fcf7b09ff84cca5
Flier Lu <flier.lu@gmail.com>
2016-04-02 01:43:44 +0800

    force to use g++4.8

--- .travis/setup-travis.sh
+++ .travis/setup-travis.sh
@@ -8,13 +8,21 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew outdated boost || brew upgrade boost
   brew install ragel
 else
+  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
   sudo apt-get update -qq
-  sudo apt-get install cmake ragel
+  sudo apt-get install -y cmake ragel g++-4.8
+
+  sudo rm -f /usr/bin/g++
+  sudo rm -f /usr/bin/gcc
+  sudo rm -f /usr/bin/gcov
+  sudo ln -s /usr/bin/g++-4.8 /usr/bin/g++
+  sudo ln -s /usr/bin/gcc-4.8 /usr/bin/gcc
+  sudo ln -s /usr/bin/gcov-4.8 /usr/bin/gcov
 
   wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz -O /tmp/boost.tar.gz
   tar -xzf /tmp/boost.tar.gz
   pushd boost_1_60_0
   ./bootstrap.sh
-  sudo ./b2 install -j 2 --prefix=/usr link=static
+  sudo ./b2 -q install -j 2 --prefix=/usr link=static
   popd
 fi
