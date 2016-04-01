54de465b996b55c0a37d92a3af029633d6bc5446
Flier Lu <flier.lu@gmail.com>
2016-04-02 00:39:10 +0800

    force to use cmake 3

--- setup-travis.sh
+++ setup-travis.sh
@@ -4,13 +4,9 @@
 
 if ( test "`uname -s`" = "Darwin" )
 then
-  #cmake v2.8.12 is installed on the Mac workers now
   brew update
   brew install cmake boost
-  echo
 else
-  #install a newer cmake since at this time Travis only has version 2.8.7
-  echo "yes" | sudo add-apt-repository ppa:kalakris/cmake
   sudo apt-get update -qq
   sudo apt-get install cmake boost
 fi
