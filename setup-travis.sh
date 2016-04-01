9aad811da3d062c8eb1f1f0e8e5a2e032300e371
Flier Lu <flier.lu@gmail.com>
2016-04-02 00:51:52 +0800

    skip installed packages

---
+++ setup-travis.sh
@@ -0,0 +1,11 @@
+#/bin/sh -f
+
+# things to do for travis-ci in the before_install section
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  #brew update
+  #brew install cmake boost
+else
+  sudo apt-get update -qq
+  sudo apt-get install cmake libboost-dev
+fi
