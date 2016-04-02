eeb6684f002b51a72e73d5fc7caa1d02874f5c39
Flier Lu <flier.lu@gmail.com>
2016-04-02 14:47:42 +0800

    user env dir for path

--- .travis/build-hyperscan.sh
+++ .travis/build-hyperscan.sh
@@ -4,7 +4,7 @@ set -e
 wget https://github.com/01org/hyperscan/archive/v4.1.0.tar.gz -O /tmp/hyperscan.tar.gz
 tar -xvf /tmp/hyperscan.tar.gz
 cd hyperscan-4.1.0
-cmake . -DCMAKE_INSTALL_PREFIX=$HOME/hyperscan -DCMAKE_POSITION_INDEPENDENT_CODE=on -DBOOST_ROOT=$HOME/boost
+cmake . -DCMAKE_POSITION_INDEPENDENT_CODE=on -DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT -DBOOST_ROOT=$BOOST_ROOT
 make
 make install
 
