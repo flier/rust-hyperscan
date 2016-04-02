3f928d862b1bb53737182a0adcd7e51dadd22a32
Flier Lu <flier.lu@gmail.com>
2016-04-02 15:28:39 +0800

    remove trailing spaces

--- .travis/build-hyperscan.sh
+++ .travis/build-hyperscan.sh
@@ -3,12 +3,11 @@ set -e
 
 if [ ! -f "$HOME/hyperscan/lib/libhs.a" ]; then
 	wget https://github.com/01org/hyperscan/archive/v4.1.0.tar.gz -O /tmp/hyperscan.tar.gz
-	tar -xvf /tmp/hyperscan.tar.gz
+	tar -xzf /tmp/hyperscan.tar.gz
 	cd hyperscan-4.1.0
-	
 	cmake . -DBOOST_ROOT=$BOOST_ROOT \
 			-DCMAKE_POSITION_INDEPENDENT_CODE=on \
-			-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT \			
+			-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT \
 			-DCMAKE_C_COMPILER=/usr/bin/gcc-4.8 \
 			-DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8
 	make
