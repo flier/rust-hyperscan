bff95ba5f6aeafae1bc89cc77561450aa7611009
Flier Lu <flier.lu@gmail.com>
2016-04-02 13:37:29 +0800

    remove symbol links

--- .travis/setup-travis.sh
+++ .travis/setup-travis.sh
@@ -9,13 +9,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew outdated boost || brew upgrade boost
   brew install ragel
 else
-  rm -f /usr/bin/g++
-  rm -f /usr/bin/gcc
-  rm -f /usr/bin/gcov
-  ln -s /usr/bin/g++-4.8 /usr/bin/g++
-  ln -s /usr/bin/gcc-4.8 /usr/bin/gcc
-  ln -s /usr/bin/gcov-4.8 /usr/bin/gcov
-
   if [ ! -d "$HOME/boost_1_60_0" ]; then
 	wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz -O /tmp/boost.tar.gz
 	tar -xzf /tmp/boost.tar.gz
