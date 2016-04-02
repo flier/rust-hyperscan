92c46c532fbd4d2638a91af996740ce3d462fedc
Flier Lu <flier.lu@gmail.com>
2016-04-02 14:44:54 +0800

    remove cache

--- .travis/build-hyperscan.sh
+++ .travis/build-hyperscan.sh
@@ -1,14 +1,10 @@
 #/bin/sh -f
 set -e
 
-if [ ! -f "$HOME/hyperscan/lib/libhs.a" ]; then
-	wget https://github.com/01org/hyperscan/archive/v4.1.0.tar.gz -O /tmp/hyperscan.tar.gz
-	tar -xvf /tmp/hyperscan.tar.gz
-	cd hyperscan-4.1.0
-	cmake . -DCMAKE_INSTALL_PREFIX=$HOME/hyperscan -DCMAKE_POSITION_INDEPENDENT_CODE=on -DBOOST_ROOT=$HOME/boost
-	make
-	make install
-else
-	echo 'Using cached hyperscan directory.';
-fi
+wget https://github.com/01org/hyperscan/archive/v4.1.0.tar.gz -O /tmp/hyperscan.tar.gz
+tar -xvf /tmp/hyperscan.tar.gz
+cd hyperscan-4.1.0
+cmake . -DCMAKE_INSTALL_PREFIX=$HOME/hyperscan -DCMAKE_POSITION_INDEPENDENT_CODE=on -DBOOST_ROOT=$HOME/boost
+make
+make install
 
--- .travis/setup-travis.sh
+++ .travis/setup-travis.sh
@@ -9,13 +9,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew outdated boost || brew upgrade boost
   brew install ragel
 else
-  if [ ! -f "$HOME/boost/include/boost/config.hpp" ]; then
-	wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz -O /tmp/boost.tar.gz
-	tar -xzf /tmp/boost.tar.gz
-    cd boost_1_60_0 
-    ./bootstrap.sh 
-    ./b2 -q -d=0 install -j 2 --prefix=$HOME/boost link=static
-  else
-  	echo 'Using cached boost directory.';
-  fi
+  wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz -O /tmp/boost.tar.gz
+  tar -xzf /tmp/boost.tar.gz
+  cd boost_1_60_0 
+  ./bootstrap.sh 
+  ./b2 -q -d=0 install -j 2 --prefix=$HOME/boost link=static
 fi
