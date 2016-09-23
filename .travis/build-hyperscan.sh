b7c42a4ea3908296df7903efbf048dad0a5e72b8
Flier Lu <flier.lu@nexusguard.com>
2016-09-23 15:49:03 +0800

    upgrade boost and hyperscan

--- .travis/build-hyperscan.sh
+++ .travis/build-hyperscan.sh
@@ -2,13 +2,13 @@
 set -e
 
 if [ ! -f "$HOME/hyperscan/lib/libhs.a" ]; then
-	wget https://github.com/01org/hyperscan/archive/v4.1.0.tar.gz -O /tmp/hyperscan.tar.gz
+	wget https://github.com/01org/hyperscan/archive/v4.3.1.tar.gz -O /tmp/hyperscan.tar.gz
 	tar -xzf /tmp/hyperscan.tar.gz
-	cd hyperscan-4.1.0
+	cd hyperscan-4.3.1
 	if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 		cmake . -DBOOST_ROOT=$BOOST_ROOT \
 				-DCMAKE_POSITION_INDEPENDENT_CODE=on \
-				-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT 
+				-DCMAKE_INSTALL_PREFIX=$HYPERSCAN_ROOT
 	else
 		cmake . -DBOOST_ROOT=$BOOST_ROOT \
 				-DCMAKE_POSITION_INDEPENDENT_CODE=on \
--- .travis/setup-travis.sh
+++ .travis/setup-travis.sh
@@ -14,14 +14,14 @@ else
 	ln -s /usr/bin/g++-4.8 $HOME/bin/g++
 	ln -s /usr/bin/gcc-4.8 $HOME/bin/gcc
 	ln -s /usr/bin/gcov-4.8 $HOME/bin/gcov
-	
+
     export PATH=$HOME/bin:$PATH
 
 	if [ ! -f "$HOME/boost/include/boost/config.hpp" ]; then
-		wget http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz -O /tmp/boost.tar.gz
+		wget http://downloads.sourceforge.net/project/boost/boost/1.61.0/boost_1_61_0.tar.gz -O /tmp/boost.tar.gz
 		tar -xzf /tmp/boost.tar.gz
-		cd boost_1_60_0 
-		./bootstrap.sh 
+		cd boost_1_61_0
+		./bootstrap.sh
 		./b2 -q -d=0 install -j 2 --prefix=$HOME/boost link=static
 	else
   		echo 'Using cached boost directory.';
