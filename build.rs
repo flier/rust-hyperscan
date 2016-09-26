49adbad1326e6b929e7838311a3b3ec0e34be1a9
Flier Lu <flier.lu@nexusguard.com>
2016-09-26 15:33:49 +0800

    generate raw.rs when it's not exists

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
+#[macro_use]
+extern crate log;
 extern crate bindgen;
 extern crate env_logger;
 
 use std::env;
+use std::path::Path;
 use std::process::Command;
 
 fn macos_sdk_path() -> String {
@@ -13,21 +16,27 @@ fn macos_sdk_path() -> String {
     String::from_utf8(output.stdout).expect("fail to decode utf8")
 }
 
-fn generate(base_dir: &str) {
+fn generate_raw_wrapper(base_dir: &str, out_file:&str) {
     let mut builder = bindgen::Builder::new(format!("{}/include/hs/hs.h", base_dir));
+
+    info!("generating raw Hyperscan wrapper {} ...", out_file);
 
     builder.link("hs", bindgen::LinkType::Static)
         .builtins()
         .clang_arg(format!("-I{}/include/hs", base_dir));
 
     if cfg!(target_os = "macos") {
-        builder.clang_arg(format!("-I{}/usr/include", macos_sdk_path()));
+        let sdk_path = macos_sdk_path();
+
+        debug!("found macOS SDK @ {}", sdk_path);
+
+        builder.clang_arg(format!("-I{}/usr/include", sdk_path));
     }
 
     builder.generate()
         .expect("Failed to generate bindings")
-        .write_to_file("src/raw.rs")
-        .expect("Failed to write file");
+        .write_to_file(out_file)
+        .expect("Failed to write generated file");
 }
 
 fn main() {
@@ -38,8 +47,15 @@ fn main() {
         Err(_) => String::from("/usr/local"),
     };
 
-    generate(&root_dir);
+    debug!("building with Hyperscan @ {}", root_dir);
 
+    let out_file = Path::new("src/raw.rs");
+
+    if !out_file.exists() {
+        generate_raw_wrapper(&root_dir, out_file.to_str().unwrap());
+    }
+
+    println!("cargo:rerun-if-changed={}/include/hs/hs.h", root_dir);
     println!("cargo:rustc-link-lib=dylib=c++");
     println!("cargo:rustc-link-lib=static=hs");
     println!("cargo:rustc-link-search=native={}/lib", root_dir);
