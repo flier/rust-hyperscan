54e1f55af0d20f39a9343a9cf5a034c9e659e4d8
Flier Lu <flier.lu@gmail.com>
2019-05-14 19:13:34 +0800

    use same binding file

--- hyperscan-sys/build.rs
+++ hyperscan-sys/build.rs
@@ -4,85 +4,89 @@ extern crate log;
 use std::env;
 use std::path::{Path, PathBuf};
 
+use failure::{err_msg, Error, ResultExt};
+
 struct Library {
     pub libs: Vec<String>,
     pub link_paths: Vec<PathBuf>,
     pub include_paths: Vec<PathBuf>,
 }
 
-fn find_hyperscan() -> Library {
-    if let Ok(prefix) = env::var("HYPERSCAN_ROOT") {
-        debug!("building with Hyperscan @ HYPERSCAN_ROOT={}", prefix);
+fn find_hyperscan() -> Result<Library, Error> {
+    env::var("HYPERSCAN_ROOT")
+        .map(|prefix| {
+            debug!("building with Hyperscan @ HYPERSCAN_ROOT={}", prefix);
 
-        Library {
-            libs: vec![From::from("hs")],
-            link_paths: vec![From::from(format!("{}/lib", prefix))],
-            include_paths: vec![From::from(format!("{}/include", prefix))],
-        }
-    } else if let Ok(pkg_config::Library {
-        libs,
-        link_paths,
-        include_paths,
-        ..
-    }) = pkg_config::Config::new().statik(true).probe("libhs")
-    {
-        debug!(
-            "building with Hyperscan @ libs={:?}, link_paths={:?}, include_paths={:?}",
-            libs, link_paths, include_paths
-        );
+            Library {
+                libs: vec!["hs".to_owned()],
+                link_paths: vec![format!("{}/lib", prefix).into()],
+                include_paths: vec![format!("{}/include", prefix).into()],
+            }
+        })
+        .or_else(|_| {
+            pkg_config::Config::new().statik(true).probe("libhs").map(
+                |pkg_config::Library {
+                     libs,
+                     link_paths,
+                     include_paths,
+                     ..
+                 }| {
+                    debug!(
+                        "building with Hyperscan @ libs={:?}, link_paths={:?}, include_paths={:?}",
+                        libs, link_paths, include_paths
+                    );
 
-        Library {
-            libs: libs,
-            link_paths: link_paths,
-            include_paths: include_paths,
-        }
-    } else {
-        panic!("please install hyperscan from https://github.com/01org/hyperscan")
-    }
+                    Library {
+                        libs,
+                        link_paths,
+                        include_paths,
+                    }
+                },
+            )
+        })
+        .or_else(|_| {
+            Err(err_msg(
+                "please download and install hyperscan from https://www.hyperscan.io/",
+            ))
+        })
 }
 
 #[cfg(feature = "gen")]
-fn generate_binding(hyperscan_include_path: &str, out_file: &Path) {
+fn generate_binding(hyperscan_include_path: &str, out_file: &Path) -> Result<(), Error> {
     info!("generating raw Hyperscan wrapper @ {}", out_file.display());
 
+    let hyperscan_include_file = format!("{}/hs.h", hyperscan_include_path);
+
     bindgen::builder()
-        .header(format!("{}/hs.h", hyperscan_include_path))
-        .clang_arg("-xc++")
-        .clang_arg("-std=c++11")
+        .header(&hyperscan_include_file)
+        .clang_args(&["-x", "c++", "-std=c++11"])
         .whitelist_function("^hs_.*")
         .generate()
-        .expect("Fail to generate bindings")
+        .map_err(|_| err_msg("generate binding files"))?
         .write_to_file(out_file)
-        .expect("Fail to write raw wrapper");
+        .context("write wrapper")?;
 
-    println!("cargo:rerun-if-changed={}/hs.h", hyperscan_include_path);
+    println!("cargo:rerun-if-changed={}", hyperscan_include_file);
+
+    Ok(())
 }
 
 #[cfg(not(feature = "gen"))]
-fn generate_binding(_: &str, out_file: &Path) {
-    use std::fs;
+fn generate_binding(_: &str, out_file: &Path) -> Result<(), Error> {
+    std::fs::copy("src/raw.rs", out_file).context("copy binding file")?;
 
-    if cfg!(target_os = "macos") {
-        fs::copy("src/macos/raw.rs", out_file).expect("fail to copy bindings");
-    } else if cfg!(target_os = "linux") {
-        fs::copy("src/linux/raw.rs", out_file).expect("fail to copy bindings");
-    } else {
-        panic!(
-            "target `{}` haven't binding file, generate it with `cargo build --features gen`",
-            env::var("TARGET").unwrap()
-        );
-    }
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     pretty_env_logger::init();
 
-    let libhs = find_hyperscan();
+    let libhs = find_hyperscan()?;
 
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR")?;
     let out_file = Path::new(&out_dir).join("raw.rs");
 
-    generate_binding(libhs.include_paths[0].to_str().unwrap(), &out_file);
+    generate_binding(libhs.include_paths[0].to_str().unwrap(), &out_file)?;
 
     for lib in libhs.libs {
         if lib.contains("hs") {
@@ -100,4 +104,6 @@ fn main() {
     for link_path in libhs.link_paths {
         println!("cargo:rustc-link-search=native={}", link_path.to_str().unwrap());
     }
+
+    Ok(())
 }
