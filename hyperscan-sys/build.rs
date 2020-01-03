6226071a9e21f2ef22d34cb9adcff8d2ec223460
Elias Levy <fearsome.lucidity@gmail.com>
2020-01-03 09:12:35 -0800

    Fix typo

--- hyperscan-sys/build.rs
+++ hyperscan-sys/build.rs
@@ -96,7 +96,7 @@ fn main() {
         }
     }
 
-    if cfg!(any(c = "macos", target_os = "freebsd")) {
+    if cfg!(any(target_os = "macos", target_os = "freebsd")) {
         println!("cargo:rustc-link-lib=dylib=c++");
     } else if cfg!(not(target_os = "windows")) {
         println!("cargo:rustc-link-lib=dylib=stdc++");
