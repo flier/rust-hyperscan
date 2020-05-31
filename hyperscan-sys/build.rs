7f0ea377a4b0b2b092fe16e5493586d4ef511038
Flier Lu <flier.lu@gmail.com>
2020-05-31 18:52:11 +0800

    generate binding file for chimera, #7

--- hyperscan-sys/build.rs
+++ hyperscan-sys/build.rs
@@ -4,7 +4,7 @@ extern crate log;
 use std::env;
 use std::path::{Path, PathBuf};
 
-use anyhow::{Context, Error};
+use anyhow::{bail, Context, Error, Result};
 
 struct Library {
     pub libs: Vec<String>,
@@ -12,16 +12,51 @@ struct Library {
     pub include_paths: Vec<PathBuf>,
 }
 
-fn find_hyperscan() -> Result<Library, Error> {
+fn find_hyperscan() -> Result<Library> {
     env::var("HYPERSCAN_ROOT")
-        .map(|prefix| {
+        .with_context(|| "HYPERSCAN_ROOT")
+        .and_then(|prefix| {
             debug!("building with Hyperscan @ HYPERSCAN_ROOT={}", prefix);
 
-            Library {
-                libs: vec!["hs".to_owned()],
-                link_paths: vec![format!("{}/lib", prefix).into()],
-                include_paths: vec![format!("{}/include/hs", prefix).into()],
+            let mut libs = vec![];
+            let mut link_paths = vec![];
+            let mut include_paths = vec![];
+
+            let prefix = Path::new(&prefix);
+            let inc_path = prefix.join("include/hs");
+            let lib_path = prefix.join("lib");
+            let libhs = lib_path.join("libhs.a");
+            let libchimera = lib_path.join("libchimera.a");
+
+            if !prefix.exists() || !prefix.is_dir() {
+                bail!("HYPERSCAN_ROOT should point to a directory that exists.");
             }
+            if lib_path.exists() && lib_path.is_dir() {
+                link_paths.push(lib_path.into())
+            } else {
+                bail!("`$HYPERSCAN_ROOT/lib` subdirectory not found.");
+            }
+            if inc_path.exists() && inc_path.is_dir() {
+                include_paths.push(inc_path.into())
+            } else {
+                bail!("`$HYPERSCAN_ROOT/include/hs` subdirectory not found.");
+            }
+            if libhs.exists() && libhs.is_file() {
+                libs.push("hs".into());
+            } else {
+                bail!("`$HYPERSCAN_ROOT/lib/libhs.a` library not found.");
+            }
+            if libchimera.exists() && libchimera.is_file() {
+                libs.push("chimera".into());
+            } else if cfg!(feature = "chimera") {
+                bail!("`$HYPERSCAN_ROOT/lib/libchimera.a` library not found.");
+            }
+
+            Ok(Library {
+                libs,
+                link_paths,
+                include_paths,
+            })
         })
         .or_else(|_| {
             pkg_config::Config::new().statik(true).probe("libhs").map(
@@ -48,15 +83,18 @@ fn find_hyperscan() -> Result<Library, Error> {
 }
 
 #[cfg(feature = "gen")]
-fn generate_binding(hyperscan_include_path: &str, out_file: &Path) -> Result<(), Error> {
+fn generate_binding(inc_dir: &Path, out_dir: &Path) -> Result<()> {
+    let out_file = out_dir.join("raw.rs");
+
     info!("generating raw Hyperscan wrapper @ {}", out_file.display());
 
-    let hyperscan_include_file = format!("{}/hs.h", hyperscan_include_path);
+    let inc_file = inc_dir.join("hs.h");
+    let inc_file = inc_file.to_str().expect("header file");
 
-    println!("cargo:rerun-if-changed={}", hyperscan_include_file);
+    println!("cargo:rerun-if-changed={}", inc_file);
 
     bindgen::builder()
-        .header(hyperscan_include_file)
+        .header(inc_file)
         .use_core()
         .ctypes_prefix("::libc")
         .clang_args(&["-x", "c++", "-std=c++11"])
@@ -74,30 +112,68 @@ fn generate_binding(hyperscan_include_path: &str, out_file: &Path) -> Result<(),
 }
 
 #[cfg(not(feature = "gen"))]
-fn generate_binding(_: &str, out_file: &Path) -> Result<(), Error> {
-    std::fs::copy("src/raw.rs", out_file)
+fn generate_binding(_: &Path, out_dir: &Path) -> Result<()> {
+    std::fs::copy("src/raw.rs", out_dir.join("raw.rs"))
         .map(|_| ())
         .with_context(|| "copy binding file")
 }
 
-fn main() -> Result<(), Error> {
+#[cfg(all(feature = "gen", feature = "chimera"))]
+fn generate_chimera_binding(inc_dir: &Path, out_dir: &Path) -> Result<()> {
+    let out_file = out_dir.join("chimera.rs");
+    let inc_file = inc_dir.join("ch.h");
+    let inc_file = inc_file.to_str().expect("header file");
+
+    println!("cargo:rerun-if-changed={}", inc_file);
+
+    bindgen::builder()
+        .header(inc_file)
+        .use_core()
+        .ctypes_prefix("::libc")
+        .clang_args(&["-x", "c++", "-std=c++11"])
+        .whitelist_var("^CH_.*")
+        .whitelist_type("^ch_.*")
+        .whitelist_function("^ch_.*")
+        .blacklist_type("^__darwin_.*")
+        .size_t_is_usize(true)
+        .derive_copy(false)
+        .derive_default(true)
+        .generate()
+        .map_err(|_| Error::msg("generate binding files"))?
+        .write_to_file(out_file)
+        .with_context(|| "write wrapper")
+}
+
+#[cfg(all(not(feature = "gen"), feature = "chimera"))]
+fn generate_chimera_binding(_: &Path, out_dir: &Path) -> Result<()> {
+    std::fs::copy("src/chimera.rs", out_dir.join("chimera.rs"))
+        .map(|_| ())
+        .with_context(|| "copy binding file")
+}
+
+fn main() -> Result<()> {
     pretty_env_logger::init();
 
     let libhs = find_hyperscan()?;
-
     let out_dir = env::var("OUT_DIR")?;
-    let out_file = Path::new(&out_dir).join("raw.rs");
+    let out_dir = Path::new(&out_dir);
+    let inc_dir = libhs.include_paths.first().expect("include path");
 
-    generate_binding(libhs.include_paths[0].to_str().unwrap(), &out_file)?;
+    generate_binding(inc_dir, &out_dir)?;
 
     for lib in libhs.libs {
-        if lib.contains("hs") {
-            println!("cargo:rustc-link-lib=dylib={}", lib);
-        }
+        println!("cargo:rustc-link-lib=dylib={}", lib);
     }
 
     for link_path in libhs.link_paths {
-        println!("cargo:rustc-link-search=native={}", link_path.to_str().unwrap());
+        println!(
+            "cargo:rustc-link-search=native={}",
+            link_path.to_str().expect("link path")
+        );
+    }
+
+    if cfg!(feature = "chimera") {
+        generate_chimera_binding(inc_dir, &out_dir)?;
     }
 
     Ok(())
--- hyperscan-sys/src/lib.rs
+++ hyperscan-sys/src/lib.rs
@@ -1,4 +1,15 @@
+//! Hyperscan is a software regular expression matching engine
+//! designed with high performance and flexibility in mind.
 #![no_std]
 #![allow(non_camel_case_types, clippy::unreadable_literal)]
 
 include!(concat!(env!("OUT_DIR"), "/raw.rs"));
+
+#[cfg(feature = "chimera")]
+pub mod chimera {
+    //! Chimera is a software regular expression matching engine that is a hybrid of Hyperscan and PCRE.
+    //!
+    //! The design goals of Chimera are to fully support PCRE syntax as well as to take advantage of
+    //! the high performance nature of Hyperscan.
+    include!(concat!(env!("OUT_DIR"), "/chimera.rs"));
+}
